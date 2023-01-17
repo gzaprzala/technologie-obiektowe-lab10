ddd779045ea408cdb7e41b41c465ea90361e6955
gzaprzala <zaprzala.grzegorz@gmail.com>
2023-01-17 23:23:05 +0100

    edited home page

--- src/main/java/com/example/lab10/App.java
+++ src/main/java/com/example/lab10/App.java
@@ -3,16 +3,19 @@ package com.example.lab10;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class App {
 
-    @RequestMapping("/")
-    @ResponseBody
+    @GetMapping("/")
     String home() {
-        return "Grzegorz Zaprzala - Technologie Obiektowe LAB10";
+        return "Grzegorz Zaprzala - Technologie Obiektowe LAB10 ----- " +
+                "Dostepne sciezki: ----- " +
+                "/listofemployees - zwraca liste pracownikow ----- " +
+                "/listofemployees/add - dodaje pracownika do listy";
     }
 
     public static void main(String[] args) {
